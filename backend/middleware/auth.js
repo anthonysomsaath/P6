b0959a8965a94f9907dad6af9daba041a15e53b3
anthonysomsaath <asomsaath@gmail.com>
2022-04-28 17:19:46 +0200

    auth added

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,12 +8,12 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.JWTPRIVATEKEY);
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId) {
+        if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID non valable';
         }   else{
             next();
         }
-    } catch (error){
+    } catch {
         res.status(403).json({error : error | 'Requête non authentifiée !'})
     }
 };
