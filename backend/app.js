9c746a967bb86ca6a8b142ccd1718387a9a330dc
anthonysomsaath <asomsaath@gmail.com>
2022-05-26 14:37:00 +0200

    helmet added

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     next();
   });
   
-app.use(helmet());
+app.use(helmet({crossOriginResourcePolicy: false}));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
